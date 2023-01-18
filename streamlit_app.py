e2267c4efa5eb999a0992df1d0960668e7adbd90
rkumar080 <122960669+rkumar080@users.noreply.github.com>
2023-01-18 15:35:58 +0530

    Update streamlit_app.py
    Added extra rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & blue berry oatmeal')
+streamlit.text('Kale Spinach')
+streamlit.text('Hard boiled Egg')
