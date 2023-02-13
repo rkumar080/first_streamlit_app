6ec0279ab763dce47f0999406de0361440eb2732
rkumar080 <122960669+rkumar080@users.noreply.github.com>
2023-02-13 17:31:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,10 @@ streamlit.text(fruityvice_response)
 streamlit.text(fruityvice_response.json())
 
 #streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
 ##streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
